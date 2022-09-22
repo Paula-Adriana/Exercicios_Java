d710febb07910f1ddd31dd08d150ebdd79af11bd
paula-adriana <paulaadrianadacosta@gmail.com>
2022-09-22 08:08:38 -0300

    treino

---
+++ ExJava/src/br/com/loiane/aula15/Ex15.java
@@ -0,0 +1,38 @@
+package br.com.loiane.aula15;
+
+import java.util.Scanner;
+
+public class Ex15 {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro lado: ");
+		int lado1 = entrada.nextInt();
+		
+		System.out.print("Digite o segundo lado: ");
+		int lado2 = entrada.nextInt();
+		
+		System.out.print("Digite o terceiro lado: ");
+		int lado3 = entrada.nextInt();
+		
+		if ((lado1 + lado2 >= lado3) || (lado2 + lado3 >= lado1) || (lado3 + lado1 >= lado2)){
+			System.out.print("É um trinângulo ");
+			if ((lado1 == lado2) && (lado1 == lado3)) {
+				System.out.println("equilátero");
+			} else if ((lado1 == lado2) || (lado1 == lado3)) {
+				System.out.println("isósceles");
+			} else {
+				System.out.println("escaleno");
+			}
+		} else {
+			System.out.println("Os três lados informados não formam um trinângulo.");
+		}
+		
+		
+		
+		
+		entrada.close();
+	
+}
+}
---
+++ ExJava/src/br/com/loiane/aula15/Ex16.java
@@ -0,0 +1,38 @@
+package br.com.loiane.aula15;
+
+import java.util.Scanner;
+
+public class Ex16 {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Digite o valor de a: ");
+		int a = entrada.nextInt();
+		if (a == 0) {
+			System.out.println("Valor de 'a' deve ser diferente de zero! Programa encerrado!");
+		} else {
+			System.out.print("Digite o valor de b: ");
+			int b = entrada.nextInt();
+
+			System.out.print("Digite o valor de c: ");
+			int c = entrada.nextInt();
+
+			double delta = Math.pow(b, 2) - (4 * a * c);
+			double x1 = ((-b) + Math.sqrt(delta)) / (2 *a);
+			double x2 = ((-b) - Math.sqrt(delta)) / (2 *a);
+
+			if (delta < 0) {
+				System.out.println("Não possui raízes reais. Delta negativo");
+			} else if (delta == 0) {
+				System.out.println("x1: " + x1);
+			} else {
+				System.out.println("x1: " + x1 + " " + "x2: " + x2);
+			}
+			
+			
+		}
+
+		entrada.close();
+	}
+}
